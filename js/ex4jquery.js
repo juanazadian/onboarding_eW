f9b89d61e4193ce65df0370d63d52a54752cefa2
jazadian <jazadian@eagerworks.com>
2021-07-27 11:26:20 -0300

    jquery exercise and fix in css

--- js/ex4jquery.js
+++ js/ex4jquery.js
@@ -1,30 +1,60 @@
+function loadLocalStorage(){
+  let wishlistItemsJSON = localStorage.getItem("wishlistItems")
+  if (!wishlistItemsJSON.length)
+    return;
+
+  wishlistItems = JSON.parse(wishlistItemsJSON);
+  var keys = Object.keys(wishlistItems);
+  var i = keys.length;
+  while ( i-- ) {
+    newrow = "<tr><td>" + keys[i] + "</td><td>" + wishlistItems[keys[i]] + '</td><td><input type="checkbox" name="done1" value="done1"></td></tr>';
+    $("table tbody").append(newrow);
+  }
+}
+
+function updateLocalstorage() {
+  let wishlistItemsJSON = localStorage.getItem("wishlistItems")
+  if (wishlistItemsJSON.length) {
+    wishlistItems = JSON.parse(wishlistItemsJSON);
+  } else {
+    wishlistItems = {};
+  }
+  wishlistItems[$("#name").val()] = $("#cost").val();
+  let newWishListItems = JSON.stringify(wishlistItems);
+  localStorage.setItem("wishlistItems", newWishListItems);
+}
+
+function checkBudget() {
+  let acc = 0;
+  let budget = parseInt($("#budget").text());
+  $("table > tbody > tr").each(function() {
+    value = parseInt($(this).children()[1].innerHTML);
+    acc += value;
+    if (acc <= budget)
+      $(this).find("input")[0].checked = true;
+  })
+}
+
+function sortRows(a, b) {
+  return parseInt(a.getElementsByTagName("td")[1].innerHTML) - parseInt(b.getElementsByTagName("td")[1].innerHTML);
+}
+
 $(document).ready(function() {
+  loadLocalStorage();
 
   $("#addbtn").click(function() {
     $("#modal").show();
   });
 
   $("#form").submit(function(event) {
+    updateLocalstorage()
     newrow = "<tr><td>" + $("#name").val() + "</td><td>" + $("#cost").val() + '</td><td><input type="checkbox" name="done1" value="done1"></td></tr>';
     $("table tbody").append(newrow);
     $("#modal").hide();
     event.preventDefault();
+    event.stopPropagation();
+    return false;
   });
-
-  function checkBudget() {
-    let acc = 0;
-    let budget = parseInt($("#budget").text());
-    $("table > tbody > tr").each(function() {
-      value = parseInt($(this).children()[1].innerHTML);
-      acc += value;
-      if (acc <= budget)
-        $(this).find("input")[0].checked = true;
-    })
-  }
-
-  function sortRows(a, b) {
-    return parseInt(a.getElementsByTagName("td")[1].innerHTML) - parseInt(b.getElementsByTagName("td")[1].innerHTML);
-  }
 
   $("#optimbtn").click(function() {
     sortedArray = Array.from($("table > tbody > tr")).sort(sortRows);
