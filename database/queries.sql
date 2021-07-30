aa4dd6a803b9b24e7f441f8dfeb6999fcd7d875a
jazadian <jazadian@eagerworks.com>
2021-07-30 13:09:36 -0300

    last part of the database exercise done

--- database/queries.sql
+++ database/queries.sql
@@ -8,11 +8,11 @@ from users LEFT JOIN (wishlist JOIN items using (itemid)) using (userid)
 select count(u1.userid)
 from users u1
 where not exists (	select 1
-				from wishlist w1
-				where w1.userid = u1.userid);
+        from wishlist w1
+        where w1.userid = u1.userid);
 
 select  u1.username
 from users u1 JOIN wishlist w1 using (userid)
 where (not w1.completed) and 5 < ( select count(f1.userid_follows)
-																		from followers f1
-																		where f1.userid_follows = u1.userid ) GROUP BY (userid);
+                                    from followers f1
+                                    where f1.userid_follows = u1.userid ) GROUP BY (userid);
